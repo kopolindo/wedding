bdf4b4c37fe40a1806eab8ced5de0599852e1538
Alex Conti <alex89.conti@gmail.com>
2024-04-11 01:29:43 +0200

    add function to insert data in db

--- api/confirm.go
+++ api/confirm.go
@@ -1,20 +1,48 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"wedding/database"
+
+	"github.com/google/uuid"
+)
 
 // HandleConfirmation handles form submission
 func handleConfirmation(w http.ResponseWriter, r *http.Request) {
 	// Retrieve form values
 	r.ParseForm()
-	guests := r.Form.Get("guests")
-	uuid := r.Form.Get("uuid")
+	numberOfGuests := r.Form.Get("guests")
+	uuidValue := r.Form.Get("uuid")
+
+	numberOfGuestsParsed, err := strconv.Atoi(numberOfGuests)
+	if err != nil {
+		log.Printf("Error during conversion of number of guests. %s\n", err.Error())
+	}
 
 	// Perform any necessary processing here
 	// For demonstration, we'll just print the values
-	println("UUID:", uuid)
-	println("Guests:", guests)
+	println("UUID:", uuidValue)
+	println("Guests:", numberOfGuests)
 
-	// You can add further logic here like saving to a database, etc.
+	uuidParsed, err := uuid.Parse(uuidValue)
+	if err != nil {
+		log.Printf("Error during UUID parsing. %s\n", err.Error())
+	}
+
+	guest := database.Guest{
+		UUID:                 uuidParsed,
+		NumberOfPartecipants: numberOfGuestsParsed,
+	}
+
+	index, err := database.InsertGuestData(guest)
+	if err != nil {
+		log.Printf("Error during database insert. %s\n", err.Error())
+	} else {
+		fmt.Println(index)
+	}
 
 	// Return a success message
 	w.Write([]byte("Confirmation received!"))
--- database/database.go
+++ database/database.go
@@ -2,12 +2,16 @@ package database
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+var db *gorm.DB
+
 func init() {
+	var err error
 	readUserPassword()
 	DSN := fmt.Sprintf(
 		DSNFORMAT,
@@ -19,12 +23,24 @@ func init() {
 	)
 
 	// Establish a connection to the MySQL database
-	db, err := gorm.Open("mysql", DSN)
+	db, err = gorm.Open("mysql", DSN)
 	if err != nil {
 		panic("failed to connect database")
 	}
-	defer db.Close()
+	//defer db.Close()
 
 	// Automigrate the schema, creating the users table if it doesn't exist
 	db.AutoMigrate(&Guest{})
+}
+
+func InsertGuestData(guest Guest) (uint, error) {
+	result := db.Create(&guest)
+	log.Printf("inserting guest: %v\n", guest)
+	index := guest.ID   // returns inserted data's primary key
+	err := result.Error // returns error
+
+	if err != nil {
+		return 0, err
+	}
+	return index, err
 }
