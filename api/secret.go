bb6271aea825a5b4b0a93650fe427f3a5cca12d7
Alex Conti <alex89.conti@gmail.com>
2024-04-24 19:34:55 +0200

    add concurrency to password search

--- api/secret.go
+++ api/secret.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 	"wedding/backend"
 	"wedding/database"
+	"wedding/models"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -13,28 +17,53 @@ import (
 // method POST
 // route /secret
 func handleSecret(c *fiber.Ctx) error {
+	start := time.Now()
 	c.Type("html")
 	// Parse form data
 	secret := c.FormValue("secret")
 	guests := database.GetAllUsers()
+	type result struct {
+		guest models.Guest
+		ok    bool
+		err   error
+	}
+
+	results := make(chan result, len(guests))
+	var wg sync.WaitGroup
 	for _, guest := range guests {
-		ok, err := backend.ComparePasswordAndHash(secret, guest.Secret)
-		if err != nil {
+		wg.Add(1)
+		go func(guest models.Guest) {
+			defer wg.Done()
+			ok, err := backend.ComparePasswordAndHash(secret, guest.Secret)
+			results <- result{guest, ok, err}
+		}(guest)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for range guests {
+		res := <-results
+		if res.err != nil {
 			return c.JSON(fiber.Map{
-				"errorMessage": err.Error(),
+				"errorMessage": res.err.Error(),
 				"statusCode":   http.StatusInternalServerError,
 			})
 		}
-		if ok {
-			location, err := url.JoinPath("/guest", guest.UUID.String())
+		if res.ok {
+			location, err := url.JoinPath("/guest", res.guest.UUID.String())
 			if err != nil {
 				return c.JSON(fiber.Map{
 					"errorMessage": err.Error(),
 					"statusCode":   http.StatusInternalServerError,
 				})
 			}
+			fmt.Println(time.Since(start).Milliseconds())
 			return c.Redirect(location)
 		}
 	}
+	fmt.Println(time.Since(start).Milliseconds())
 	return c.Redirect("/chisono")
 }
