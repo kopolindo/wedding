0d8619211f8517ba2ca8f547d3525b9c7aee0a83
Alex Conti <alex89.conti@gmail.com>
2024-05-04 15:18:11 +0200

    fix old imports in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router} from "react-router-dom";
 
 import Header from './components/Header'
@@ -13,9 +13,9 @@ const Routing = () => {
 }
 
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Routing />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
