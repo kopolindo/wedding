443a51b9a4dfa75e74744f2ff4dc18f4a7d49390
Alex Conti <alex89.conti@gmail.com>
2024-05-10 01:36:16 +0200

    deleted useless files and imports

--- my-app/src/components/Body.jsx
+++ my-app/src/components/Body.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import './body.css';
 
