64ee39fe13ab90fcf7eccf20dfc09b0202637d7f
Alex Conti <alex89.conti@gmail.com>
2024-06-16 19:15:30 +0200

    removed direxists function

--- backend/log/helpers.go
+++ backend/log/helpers.go
@@ -6,15 +6,6 @@ import (
 	"strings"
 )
 
-// dirExists checks if directory exists, return true if exists, false otherwise
-func dirExists(dirname string) bool {
-	info, err := os.Stat(dirname)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return err == nil && info.IsDir()
-}
-
 // ensureDir checks if directory exists, if not it creates it
 func ensureDir(dirname string) error {
 	info, err := os.Stat(dirname)
