32add6151323a151a97218d9542907db76d4dcf7
Alex <alex89.conti@gmail.com>
2024-07-27 14:31:07 +0200

    add failed validation message

--- backend/api/confirm.go
+++ backend/api/confirm.go
@@ -1,8 +1,8 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
 	"time"
 	"wedding/database"
 	"wedding/log"
@@ -12,6 +12,14 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
+
+type ValidationErrorJSON struct {
+	Errors []struct {
+		FailedField string `json:"failed_field"`
+		Tag         string `json:"tag"`
+		Value       string `json:"value"`
+	} `json:"errors"`
+}
 
 func (v XValidator) Validate(data interface{}) []ErrorResponse {
 	validationErrors := []ErrorResponse{}
@@ -51,13 +59,20 @@ func handleFormPost(c *fiber.Ctx) error {
 			JSON(fiber.Map{"errorMessage": err.Error()})
 	}
 
+	firstName, lastName, err := database.GetMainUserByUUID(uuid)
+	if err != nil {
+		log.Errorf("error during fetching of user details from UUID %s: %s", uuid, err.Error())
+	}
+
 	data := new(JSONConfirmationForm)
 
 	if err := c.BodyParser(data); err != nil {
-		log.Errorf("error during JSON parsing: %s\n", err.Error())
+		log.Errorf("error during JSON parsing: %s", err.Error())
 		return c.Status(fiber.StatusInternalServerError).
 			JSON(fiber.Map{"errorMessage": err.Error()})
 	}
+
+	log.Debugf("received this data from %s %s (%s): %v", firstName, lastName, uuid, data)
 
 	var guests []models.Guest
 	// Create guest struct from parsed data
@@ -72,20 +87,36 @@ func handleFormPost(c *fiber.Ctx) error {
 		}
 		// Validation
 		if errs := myValidator.Validate(&guest); len(errs) > 0 && errs[0].Error {
-			errMsgs := make([]string, 0)
+			var validationErrorJSON ValidationErrorJSON
 
 			for _, err := range errs {
-				errMsgs = append(errMsgs, fmt.Sprintf(
-					"[%s]: '%v' needs to implement '%s'",
-					err.FailedField,
-					err.Value,
-					err.Tag,
-				))
+				validationErrorJSON.Errors = append(validationErrorJSON.Errors, struct {
+					FailedField string `json:"failed_field"`
+					Tag         string `json:"tag"`
+					Value       string `json:"value"`
+				}{
+					FailedField: err.FailedField,
+					Tag:         err.Tag,
+					Value:       fmt.Sprintf("%s", err.Value),
+				})
+			}
+
+			for _, err := range validationErrorJSON.Errors {
+				log.Errorf("validation error: [%s]: '%v' needs to implement '%s'", err.FailedField, err.Value, err.Tag)
+			}
+
+			errorJSON, err := json.Marshal(validationErrorJSON)
+			if err != nil {
+				log.Errorf("error marshaling validation errors: %v", err)
+				return &fiber.Error{
+					Code:    fiber.StatusInternalServerError,
+					Message: "Internal Server Error",
+				}
 			}
 
 			return &fiber.Error{
 				Code:    fiber.ErrBadRequest.Code,
-				Message: strings.Join(errMsgs, "\n"),
+				Message: string(errorJSON),
 			}
 		}
 		guests = append(guests, guest)
@@ -95,7 +126,7 @@ func handleFormPost(c *fiber.Ctx) error {
 		if database.GuestExists(guest.ID, guest.UUID) {
 			err = database.UpdateGuest(guest)
 			if err != nil {
-				log.Errorf("error after updating guest: %s\n", err.Error())
+				log.Errorf("error after updating guest: %s", err.Error())
 				return c.Status(fiber.StatusInternalServerError).
 					JSON(fiber.Map{"errorMessage": err.Error()})
 			}
@@ -111,13 +142,14 @@ func handleFormPost(c *fiber.Ctx) error {
 			guest.ID = 0
 			_, err := database.CreateGuest(guest)
 			if err != nil {
-				log.Errorf("error during guest creation: %s\n", err.Error())
+				log.Errorf("error during guest creation: %s", err.Error())
 				return c.Status(fiber.StatusInternalServerError).
 					JSON(fiber.Map{"errorMessage": err.Error()})
 			}
 		}
 	}
 
+	log.Infof("POST request correctly handled for user: %s, %s, %s", uuid, firstName, lastName)
 	return c.Status(fiber.StatusOK).
 		JSON(fiber.Map{"status": "ok"})
 }
