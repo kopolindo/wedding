c19fa6ccc38331194a75fe96b8ec1e9e6ddb2707
Alex <alex89.conti@gmail.com>
2024-07-27 14:31:54 +0200

    fix logging to only print to one file

--- backend/log/log.go
+++ backend/log/log.go
@@ -6,63 +6,82 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"os/user"
 	"path/filepath"
+	"syscall"
 )
 
 var (
 	textLogger         *slog.Logger
-	jsonLogger         *slog.Logger
 	slogHandlerOptions *slog.HandlerOptions
 	textHandler        *slog.TextHandler
-	jsonHandler        *slog.JSONHandler
 	logLevel           *slog.LevelVar
 )
 
 const (
 	loggerFileName     string = "backend-logs.log"
 	jsonLoggerFileName string = "backend-logs.json"
-	LOGDIRDOCKER       string = "/var/log/backend"
-	LOGDIR             string = "../backend-logs"
+	LOGDIRDOCKER       string = "/tmp/logs"
 )
 
+func printFolderDetails(folderPath string) {
+	fileInfo, err := os.Stat(folderPath)
+	if err != nil {
+		log.Fatalf("Error retrieving folder info: %v", err)
+	}
+
+	// Type assertion to get the underlying data structure.
+	stat, ok := fileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		log.Fatalf("Error getting file stats")
+	}
+
+	// Get the owner and group IDs
+	uid := stat.Uid
+	gid := stat.Gid
+
+	// Get user details
+	owner, err := user.LookupId(fmt.Sprint(uid))
+	if err != nil {
+		log.Fatalf("Error looking up user: %v", err)
+	}
+
+	// Get group details
+	group, err := user.LookupGroupId(fmt.Sprint(gid))
+	if err != nil {
+		log.Fatalf("Error looking up group: %v", err)
+	}
+
+	fmt.Printf("Folder: %s\n", folderPath)
+	fmt.Printf("Owner: %s (%s)\n", owner.Username, owner.Name)
+	fmt.Printf("Group: %s (%s)\n", group.Name, group.Gid)
+	fmt.Printf("Permissions: %s\n", fileInfo.Mode())
+	fmt.Printf("Size: %d bytes\n", fileInfo.Size())
+	fmt.Printf("Last modified: %s\n", fileInfo.ModTime())
+}
+
+func printWhoAmI() {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("Error fetching current user: %v", err)
+	}
+	fmt.Printf("Current user: %s\n", currentUser.Username)
+}
+
 func init() {
+	printWhoAmI()
+	printFolderDetails(LOGDIRDOCKER)
 	slogHandlerOptions = &slog.HandlerOptions{}
 	logLevel = &slog.LevelVar{} // INFO
-	var logDir string
-	if runningInDocker() {
-		err := ensureDir(LOGDIRDOCKER)
-		if err != nil {
-			log.Println("error during directory creation")
-		}
-		logDir = LOGDIRDOCKER
-	} else {
-		err := ensureDir(LOGDIR)
-		if err != nil {
-			log.Println("error during directory creation")
-		}
-		logDir = LOGDIR
-	}
-	loggerFilePath := filepath.Join(logDir, loggerFileName)
-	jsonLoggerFilePath := filepath.Join(logDir, jsonLoggerFileName)
-	loggerFile, err := os.OpenFile(loggerFilePath, os.O_CREATE|os.O_APPEND, 0600)
+	logFilePath := filepath.Join(LOGDIRDOCKER, loggerFileName)
+	logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Println("error during log file creation/opening:", err.Error())
+		log.Fatalf("Failed to open log file: %v", err)
 	}
-	jsonLoggerFile, err := os.OpenFile(jsonLoggerFilePath, os.O_CREATE|os.O_APPEND, 0600)
-	if err != nil {
-		log.Println("error during log file creation/opening:", err.Error())
-	}
-
 	slogHandlerOptions.Level = logLevel
-
-	loggerWriter := io.Writer(loggerFile)
-	jsonLoggerWriter := io.Writer(jsonLoggerFile)
-
+	loggerWriter := io.Writer(logFile)
 	textHandler = slog.NewTextHandler(loggerWriter, slogHandlerOptions)
-	jsonHandler = slog.NewJSONHandler(jsonLoggerWriter, slogHandlerOptions)
-
 	textLogger = slog.New(textHandler)
-	jsonLogger = slog.New(jsonHandler)
 }
 
 func SetSlogLevel(level slog.Level) {
@@ -74,33 +93,27 @@ func GetSlogLevel() slog.Level {
 
 func Info(errorMessage error) {
 	textLogger.Info(errorMessage.Error())
-	jsonLogger.Info(errorMessage.Error())
 }
 
 func Debug(errorMessage error) {
 	textLogger.Debug(errorMessage.Error())
-	jsonLogger.Debug(errorMessage.Error())
 }
 
 func Error(errorMessage error) {
 	textLogger.Error(errorMessage.Error())
-	jsonLogger.Error(errorMessage.Error())
 }
 
 func Infof(format string, a ...any) {
 	errorMessage := fmt.Sprintf(format, a...)
 	textLogger.Info(errorMessage)
-	jsonLogger.Info(errorMessage)
 }
 
 func Debugf(format string, a ...any) {
 	errorMessage := fmt.Sprintf(format, a...)
 	textLogger.Debug(errorMessage)
-	jsonLogger.Debug(errorMessage)
 }
 
 func Errorf(format string, a ...any) {
 	errorMessage := fmt.Sprintf(format, a...)
 	textLogger.Error(errorMessage)
-	jsonLogger.Error(errorMessage)
 }
